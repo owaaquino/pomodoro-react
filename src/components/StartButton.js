b35d7a5dec4db42e79ae1d595c39c421eadfe32f
owaaquino <josejoshuaaquino@gmail.com>
2018-10-06 07:14:38 +0800

    added some start button functionality. In progress

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 class StartButton extends React.Component {
+  state = {
+    countdown: 0
+  };
+
+  timer = seconds => {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+
+    setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      if (secondsLeft <= 0) {
+        clearInterval(this.state.countdown);
+      }
+
+      // this.setState((countdown: secondsLeft));
+      console.log(secondsLeft);
+    }),
+      1000;
+  };
+
   startTimer = e => {
+    const timerNow = this.timer(10);
+    this.props.timerStart(timerNow);
     console.log("beep!");
   };
 
